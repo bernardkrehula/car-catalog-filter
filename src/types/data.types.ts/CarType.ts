6f388b62b0de25d4b3582009abd2b8bda2a9a044
Bernard Krehula <krehula.bernard456@gmail.com>
2026-04-23 20:15:21 +0200

    feat(types): define car type

---
+++ src/types/data.types.ts/CarType.ts
@@ -0,0 +1,14 @@
+export type CarType = {
+  id: number;
+  brand: string;
+  model: string;
+  year: number;
+  price: number;
+  fuel_type: "Petrol" | "Diesel" | "Electric" | "Hybrid";
+  transmission: "Manual" | "Automatic" | "Semi-auto";
+  mileage: number;
+  body_type: "Sedan" | "SUV" | "Hatchback" | "Coupe" | "Van";
+  engine_size: string;
+  color: string;
+}
+ 
