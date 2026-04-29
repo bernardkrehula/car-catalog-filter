eab3835709fb61c059111de682825c44ac14ab54
Bernard Krehula <krehula.bernard456@gmail.com>
2026-04-29 21:09:04 +0200

    feat(type): add filterOptionsType

---
+++ src/types/data.types.ts/FilterOptionsType.ts
@@ -0,0 +1,12 @@
+export type FilterOptionsType = {
+  brand: string[];
+  model: string[];
+  year: number[];
+  price: string[];
+  fuel_type: string[];
+  transmission: string[];
+  mileage: string[];
+  body_type: string[];
+  engine_size: string[];
+  color: string[];
+};
